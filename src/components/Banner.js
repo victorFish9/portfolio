12bfa16832e52726d3cc113773d6fe6b8c3c982f
Victor <aue228lublupacanov@gmail.com>
2024-08-08 17:10:41 +0200

    icons switch  added to banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,53 +4,23 @@ import { useState, useEffect } from "react"
 import headerImg from '../photo/header-img.png'
 
 export const Banner = () => {
-    const [loopNum, setLoopNum] = useState(0)
-    const [isDeleting, setIsDeleting] = useState(false)
-    const toRotate = ["Web developer", "Web Designer", "UI/UX Designer"]
-    const [text, setText] = useState('')
-    const [delta, setDelta] = useState(300 - Math.random() * 100)
-    const period = 2000
     const icons = [
         "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original.svg",
-        "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg"
+        "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-plain.svg",
+        "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/java/java-plain.svg"
     ]
     const [currentIconIndex, setcurrentIconIndex] = useState(0)
 
     useEffect(() => {
-        let ticker = setInterval(() => {
-            tick()
-        }, delta)
-
         const imageTicker = setInterval(() => {
             setcurrentIconIndex((prevIndex) => (prevIndex + 1) % icons.length)
-        }, 1000)
+        }, 5000)
 
         return () => {
-            clearInterval(ticker)
             clearInterval(imageTicker)
         }
-    }, [text])
+    })
 
-    const tick = () => {
-        let i = loopNum % toRotate.length
-        let fullText = toRotate[i]
-        let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1)
-
-        setText(updatedText)
-
-        if (isDeleting) {
-            setDelta(prevDelta => prevDelta / 2)
-        }
-
-        if (!isDeleting && updatedText === fullText) {
-            setIsDeleting(true)
-            setDelta(period)
-        } else if (isDeleting && updatedText === '') {
-            setIsDeleting(false)
-            setLoopNum(loopNum + 1)
-            setDelta(500)
-        }
-    }
 
     return (
         <section className="banner" id="home">
@@ -60,7 +30,7 @@ export const Banner = () => {
                         <span className="tagline">
                             Welcome to my portfolio
                         </span>
-                        <h1>{'Hi I am Victor '}<span className="wrap">{text}</span></h1>
+                        <h1>{'Hi I am Victor '}<span className="wrap">Web developer</span></h1>
                         <p>simple lorem simple loremsimple loremsimple loremsimple loremsimple loremsimple lorem</p>
                         <button onClick={() => console.log("connect")}>Let's connect<ArrowRightCircle size={25} /></button>
                     </Col>
