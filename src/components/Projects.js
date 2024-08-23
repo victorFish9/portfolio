187dd40bbd3bc2e4e192fe214cab28cc88f6f3a2
Victor <aue228lublupacanov@gmail.com>
2024-08-23 12:45:45 +0200

    clasification added for tabs

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -8,26 +8,9 @@ import projects1 from '../projects-file'
 
 export const Projects = () => {
 
-    const projects = [
-        {
-            id: "1",
-            title: "Business Startup",
-            description: "Design & Development",
-            imgUrl: "https://t4.ftcdn.net/jpg/02/38/40/55/360_F_238405533_yyX4K55OmH2LiM7LQ01ITn6kl0b6J6K8.jpg",
-        },
-        {
-            id: "2",
-            title: "Business End",
-            description: "Design & Development",
-            imgUrl: "https://t4.ftcdn.net/jpg/02/38/40/55/360_F_238405533_yyX4K55OmH2LiM7LQ01ITn6kl0b6J6K8.jpg",
-        },
-        {
-            id: "3",
-            title: "Business Expansion",
-            description: "Marketing & Strategy",
-            imgUrl: "https://t4.ftcdn.net/jpg/02/38/40/55/360_F_238405533_yyX4K55OmH2LiM7LQ01ITn6kl0b6J6K8.jpg",
-        }
-    ];
+    const filterProjects = (language) => {
+        return projects1.filter(project => project.language === language)
+    }
 
     return (
         <section className="project" id="projects">
@@ -44,20 +27,20 @@ export const Projects = () => {
                         <Tab.Container id="projects-tabs" defaultActiveKey="first">
                             <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
                                 <Nav.Item>
-                                    <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                                    <Nav.Link eventKey="first">JavaScript</Nav.Link>
                                 </Nav.Item>
                                 <Nav.Item>
-                                    <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                                    <Nav.Link eventKey="second">Java</Nav.Link>
                                 </Nav.Item>
                                 <Nav.Item>
-                                    <Nav.Link eventKey="third">Tab 3</Nav.Link>
+                                    <Nav.Link eventKey="third">Python</Nav.Link>
                                 </Nav.Item>
                             </Nav>
                             <Tab.Content>
                                 <Tab.Pane eventKey="first">
                                     <Row>
                                         {
-                                            projects1.map((project, index) => (
+                                            filterProjects('JavaScript').map((project, index) => (
                                                 <ProjectCard key={index} {...project} />
                                             ))
                                         }
@@ -66,7 +49,7 @@ export const Projects = () => {
                                 <Tab.Pane eventKey="second">
                                     <Row>
                                         {
-                                            projects1.map((project, index) => (
+                                            filterProjects('Java').map((project, index) => (
                                                 <ProjectCard key={index} {...project} />
                                             ))
                                         }
@@ -75,7 +58,7 @@ export const Projects = () => {
                                 <Tab.Pane eventKey="third">
                                     <Row>
                                         {
-                                            projects1.map((project, index) => (
+                                            filterProjects('Python').map((project, index) => (
                                                 <ProjectCard key={index} {...project} />
                                             ))
                                         }
--- src/projects-file.js
+++ src/projects-file.js
@@ -1,31 +1,34 @@
 const projects = [
     {
         id: "1",
-        title: "Business Startup",
+        title: "JavaScript Project",
         description: "Design & Development",
         imgUrl: "https://t4.ftcdn.net/jpg/02/38/40/55/360_F_238405533_yyX4K55OmH2LiM7LQ01ITn6kl0b6J6K8.jpg",
         images: [
             "https://t4.ftcdn.net/jpg/02/38/40/55/360_F_238405533_yyX4K55OmH2LiM7LQ01ITn6kl0b6J6K8.jpg",
             "https://images.pexels.com/photos/1024975/pexels-photo-1024975.jpeg?cs=srgb&dl=pexels-asadphoto-1024975.jpg&fm=jpg"
         ],
+        language: "JavaScript"
     },
     {
         id: "2",
-        title: "Business End",
+        title: "Java Project",
         description: "Design & Development",
         imgUrl: "https://t4.ftcdn.net/jpg/02/38/40/55/360_F_238405533_yyX4K55OmH2LiM7LQ01ITn6kl0b6J6K8.jpg",
         images: [
             "https://t4.ftcdn.net/jpg/02/38/40/55/360_F_238405533_yyX4K55OmH2LiM7LQ01ITn6kl0b6J6K8.jpg",
-        ]
+        ],
+        language: "Java"
     },
     {
         id: "3",
-        title: "Business Start",
+        title: "Pyhton Project",
         description: "Design & Development",
         imgUrl: "https://t4.ftcdn.net/jpg/02/38/40/55/360_F_238405533_yyX4K55OmH2LiM7LQ01ITn6kl0b6J6K8.jpg",
         images: [
             "https://t4.ftcdn.net/jpg/02/38/40/55/360_F_238405533_yyX4K55OmH2LiM7LQ01ITn6kl0b6J6K8.jpg",
-        ]
+        ],
+        language: "Python"
     },
     {
         id: "4",
