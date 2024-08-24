0e67e52af1be345506c29d3be19c1d84b298f4c9
Victor <aue228lublupacanov@gmail.com>
2024-08-24 21:52:42 +0200

    added java project

--- src/projects-file.js
+++ src/projects-file.js
@@ -1,8 +1,14 @@
 import esnLogo from './photo/web-FI-HELS-KAN-colour-black-with-white-background.jpeg'
+
 import esnScreen1 from './photo/esnScreen1.png'
 import esnScreen2 from './photo/esnScreen2.png'
 import esnScreen3 from './photo/esnScreen3.png'
 import esnScreen4 from './photo/esnScreen4.png'
+
+import dogStoreScreen1 from './photo/dogStoreScreen1.png'
+import dogStoreScreen2 from './photo/dogStoreScreen2.png'
+import dogStoreScreen3 from './photo/dogStoreScreen3.png'
+import dogStoreScreen4 from './photo/dogStoreScreen4.png'
 
 
 const projects = [
@@ -23,11 +29,14 @@ const projects = [
     },
     {
         id: "2",
-        title: "Java Project",
-        description: "I developed a full-stack e-commerce website for a dog store as part of a university team project, creating a back-end API to manage products, users, customers, and manufacturers. The Main API serves as a central hub, linking to resources like products, manufacturers, types, customers, and users. The User Management API handles user registration, authentication, and roles, including creating, updating, and deleting profiles. The Manufacturer API manages manufacturer details and associated products. The Product Type API categorizes products for easy filtering. The Customer API manages customer profiles and supports searching. This project built a comprehensive API for efficient management of the store’s key business data.",
-        imgUrl: "https://t4.ftcdn.net/jpg/02/38/40/55/360_F_238405533_yyX4K55OmH2LiM7LQ01ITn6kl0b6J6K8.jpg",
+        title: "Dog Store Website",
+        description: "I developed a full-stack e-commerce website for a dog store as part of a university team project, using Java Spring Boot for the back-end API and React for the front-end. The API manages products, users, customers, and manufacturers. The Main API serves as a central hub, linking to resources like products, manufacturers, types, customers, and users. The User Management API handles user registration, authentication, and roles, including creating, updating, and deleting profiles. The Manufacturer API manages manufacturer details and associated products. The Product Type API categorizes products for easy filtering. The Customer API manages customer profiles and supports searching. We also created a simple front-end to display products from the API and support user registration, effectively combining the back-end and front-end for a seamless demonstration.",
+        imgUrl: dogStoreScreen1,
         images: [
-            "https://t4.ftcdn.net/jpg/02/38/40/55/360_F_238405533_yyX4K55OmH2LiM7LQ01ITn6kl0b6J6K8.jpg",
+            dogStoreScreen1,
+            dogStoreScreen3,
+            dogStoreScreen4,
+            dogStoreScreen2,
         ],
         sourceCode: "https://github.com/orgs/Ohjelmistoprojekti-1-Ryhma-2/repositories",
         language: "Java"
